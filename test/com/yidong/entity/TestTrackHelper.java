1cd24c1a238e00e03fbd366b3a1bc308084e93c5
yixun <ekixun@localhost>
2015-04-11 17:55:24 +0800

    ADD: add junit test.

---
+++ test/com/yidong/entity/TestTrackHelper.java
@@ -0,0 +1,42 @@
+package com.yidong.entity;
+
+import static org.junit.Assert.*;
+
+import java.util.Date;
+
+import org.junit.Test;
+
+import com.edong.entity.TrackHelper;
+import com.edong.entity.TrackPoint;
+
+public class TestTrackHelper {
+	
+	/**
+	 * testCase
+	 * 格式是: 纬度1，经度1，纬度2，经度2，距离答案
+	 */
+	private static double gpsInfo[][] = {
+		{39.91667, 116.41667, 30.51667, 114.31667, 1062403.2024982641},
+		{39.91667, 116.41667, 39.91667, 116.41667, 0.0},
+		{30.51667, 114.31667, 30.51667, 114.31667, 0.0},
+		{34.0522, -118.2436, 35.6850, 139.7514, 8811322.3764383}
+	};
+
+	@Test
+	public void testGetHorizonDist() {
+		
+		for (int i = 0; i < gpsInfo.length; i++) {
+			TrackPoint tp1 = new TrackPoint(gpsInfo[i][0], gpsInfo[i][1], 0, new Date());
+			TrackPoint tp2 = new TrackPoint(gpsInfo[i][2], gpsInfo[i][3], 0, new Date());
+			double distance = TrackHelper.getHorizonDist(tp1, tp2);
+			System.out.println("Calc tp1=" + tp1 + " and tp2=" + tp2 
+					+ ", Distance=" + distance + " VS. expectedDistance=" + gpsInfo[i][4] );
+			assertEquals(gpsInfo[i][4], distance, 0);
+			distance = TrackHelper.getHorizonDist(tp2, tp1);
+			System.out.println("Calc tp2=" + tp2 + " and tp1=" + tp1 
+					+ ", Distance=" + distance + " VS. expectedDistance=" + gpsInfo[i][4] );
+			assertEquals(gpsInfo[i][4], distance, 0);
+		}
+	}
+
+}
