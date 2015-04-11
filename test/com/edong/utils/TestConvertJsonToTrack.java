ddf18ba20e60048af0092976de4bbadbe1ea15ca
yixun <ekixun@localhost>
2015-04-11 22:56:03 +0800

    MOD: modify testcase

---
+++ test/com/edong/utils/TestConvertJsonToTrack.java
@@ -0,0 +1,46 @@
+package com.edong.utils;
+
+import static org.junit.Assert.*;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import com.edong.entity.Track;
+import com.edong.entity.TrackHelper;
+import com.edong.entity.TrackSummary;
+
+public class TestConvertJsonToTrack {
+
+	@Test
+	public void testConvertJsonToTrack() throws IOException {
+		System.out.println("Load track from json file: " 
+	       + new File(TestConvertTrackToJson.TestJSONFiles[0]).getAbsolutePath());
+		Track track = JsonFormatConverter.convertJsonToTrack(TestConvertTrackToJson.TestJSONFiles[0]);
+		System.out.println("point size=" + track.getLocations().size());
+		TrackSummary trackSummary = TrackHelper.sumTrack(track);
+		double expectedSumUp = 1287.3970000000002;
+		double expectedSumDown = 1217.029000000001;
+		double expectedSumUpDist = 20041.425682856945;
+		double expectedSumDownDist = 23441.48697412073;
+		assertEquals(expectedSumUp, trackSummary.getSumUp(), 0);
+		assertEquals(expectedSumDown, trackSummary.getSumDown(), 0);
+		assertEquals(expectedSumUpDist, trackSummary.getSumUpDistance(), 0);
+		assertEquals(expectedSumDownDist, trackSummary.getSumDownDistance(), 0);
+		System.out.println(trackSummary);
+	}
+	
+	@Test
+	public void testConvertJsonToTracks() throws IOException {
+		for (int i = 0; i < TestConvertTrackToJson.TestJSONFiles.length; i++) {
+			System.out.println("Load track from json file: " 
+				       + new File(TestConvertTrackToJson.TestJSONFiles[i]).getAbsolutePath());
+			Track track = JsonFormatConverter.convertJsonToTrack(TestConvertTrackToJson.TestJSONFiles[i]);
+			System.out.println("point size=" + track.getLocations().size());
+			TrackSummary trackSummary = TrackHelper.sumTrack(track);	
+			System.out.println(trackSummary);
+		}
+	}
+
+}
---
+++ test/com/edong/utils/TestConvertTrackToJson.java
@@ -0,0 +1,50 @@
+package com.edong.utils;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.text.ParseException;
+
+import net.sf.json.JSONObject;
+
+import org.dom4j.DocumentException;
+import org.junit.Test;
+
+import com.edong.entity.GPXParser;
+import com.edong.entity.Track;
+
+public class TestConvertTrackToJson {
+	
+	public static final String TestGpxFiles[] = {
+		"data/1235m.gpx", "data/1319m.gpx", "data/1880m.gpx", "data/2161m.gpx",
+		"data/2779m.gpx", "data/439m.gpx", "data/884m.gpx", "data/mini.gpx"
+	};
+	public static final String TestJSONFiles[] = {
+		"data/1235m.json", "data/1319m.json", "data/1880m.json", "data/2161m.json",
+		"data/2779m.json", "data/439m.json", "data/884m.json", "data/mini.json"
+	};
+
+	@Test
+	public void testConvertTrackToJson() throws DocumentException, ParseException, FileNotFoundException {
+		for (int i = 0; i < TestGpxFiles.length; i++) {
+			System.out.println("Load track form gpx file: " + new File(TestGpxFiles[i]).getAbsolutePath());
+			GPXParser gpxParser = new GPXParser();
+			Track track = gpxParser.parse(TestGpxFiles[i]);
+			JSONObject jsonObj = JsonFormatConverter.convertTrackToJson(track);
+			PrintWriter pw = null;
+			try {
+				pw = new PrintWriter(new OutputStreamWriter(new FileOutputStream(
+						new File(TestJSONFiles[i]))),true);  
+				pw.println(jsonObj);
+			} finally {
+				if (pw != null) {
+					pw.close();
+				}
+			}
+			System.out.println("Write track to json file: " + new File(TestJSONFiles[i]).getAbsolutePath());
+		}
+	}
+
+}
